91c133390133a19a96034bc2ae26a2dfd1400638
KajalPatel2896 <kajolpatel28@gmail.com>
2023-09-29 19:57:44 -0400

    make some changes

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ let errors = require('restify-errors');
 let restify = require('restify')
 
 // Get a persistence engine for the products
-, productsSave = require('save')('products')
+    , productsSave = require('save')('products')
 
-// Create the restify server
-, server = restify.createServer({ name: SERVER_NAME})
+    // Create the restify server
+    , server = restify.createServer({ name: SERVER_NAME})
 
-server.listen(PORT, HOST, function () {
-console.log('Server %s listening at %s', server.name, server.url)
-console.log('**** Resources: ****')
-console.log('********************')
-console.log(' /products')
-console.log(' /products/:id')  
+    server.listen(PORT, HOST, function () {
+    console.log('Server %s listening at %s', server.name, server.url)
+    console.log('**** Resources: ****')
+    console.log('********************')
+    console.log(' /products')
+    console.log(' /products/:id')  
 })
 
 server.use(restify.plugins.fullResponse());
@@ -24,34 +24,34 @@ server.use(restify.plugins.bodyParser());
 
 // Get all products in the system
 server.get('/products', function (req, res, next) {
-console.log('GET /products params=>' + JSON.stringify(req.params));
+    console.log('GET /products params=>' + JSON.stringify(req.params));
 
-// Find every entity within the given collection
-productsSave.find({}, function (error, products) {
+    // Find every entity within the given collection
+    productsSave.find({}, function (error, products) {
 
-  // Return all of the products in the system
-  res.send(products)
-})
+    // Return all of the products in the system
+    res.send(products)
+    })
 })
 
 // Get a single product by their product id
 server.get('/products/:id', function (req, res, next) {
-console.log('GET /products/:id params=>' + JSON.stringify(req.params));
+    console.log('GET /products/:id params=>' + JSON.stringify(req.params));
 
-// Find a single product by their id within save
-productsSave.findOne({ product_id: req.params.id }, function (error, product) {
+    // Find a single product by their id within save
+    productsSave.findOne({ product_id: req.params.id }, function (error, product) {
 
-  // If there are any errors, pass them to next in the correct format
-  if (error) return next(new Error(JSON.stringify(error.errors)))
+        // If there are any errors, pass them to next in the correct format
+        if (error) return next(new Error(JSON.stringify(error.errors)))
 
-  if (product) {
-    // Send the product if no issues
-    res.send(product)
-  } else {
-    // Send 404 header if the product doesn't exist
-    res.send(404)
-  }
-})
+        if (product) {
+            // Send the product if no issues
+            res.send(product)
+        } else {
+            // Send 404 header if the product doesn't exist
+            res.send(404)
+        }
+    })
 })
 
 // Create a new product
@@ -138,17 +138,4 @@ server.del('/products/:id', function (req, res, next) {
       res.send(204)
     })
 })
-// Delete product with the given id
-server.del('/products/:id', function (req, res, next) {
-    console.log('POST /products params=>' + JSON.stringify(req.params));
-    // Delete the product with the persistence engine
-    productsSave.delete(req.params.id, function (error, product) {
-  
-      // If there are any errors, pass them to next in the correct format
-      if (error) return next(new Error(JSON.stringify(error.errors)))
-  
-      // Send a 204 response
-      res.send(204)
-    })
-  })
   
